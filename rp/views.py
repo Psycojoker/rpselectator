2924271ba38b8456cb321d2e6b7215d6585b4625
Laurent Peuch <cortex@worlddomination.be>
2011-09-27 22:37:36 +0200

    [enh] import urls from log-archive

--- rp/views.py
+++ rp/views.py
@@ -7,10 +7,16 @@ from models import RP
 from urllib2 import urlopen
 
 def fill(request):
+    def get_urls(url):
+        a = 0
+        for url in urlopen(url):
+            url = url[:-1]
+            if not RP.objects.filter(url=url):
+                RP.objects.create(url=url)
+                a += 1
+        return a
+
     a = 0
-    for url in urlopen("http://nurpa.be/log"):
-        url = url[:-1]
-        if not RP.objects.filter(url=url):
-            RP.objects.create(url=url)
-            a += 1
+    a += get_urls("http://nurpa.be/log")
+    a += get_urls("http://nurpa.be/log-archive")
     return HttpResponse("%s new rp items" % a)
