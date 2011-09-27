0790f7e5ba53326c8860d76642230f8845719edd
Laurent Peuch <cortex@worlddomination.be>
2011-09-27 22:21:01 +0200

    [enh] add RP model

---
+++ rp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class RP(models.Model):
+    title = models.CharField(max_length=300)
+    url = models.URLField()
+    published = models.BooleanField(default=False)
+    note = models.IntegerField(default=0)
+
+# Create your models here.
