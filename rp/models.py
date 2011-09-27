5600c13acce744085302b19826a1299aa9aeab2c
Laurent Peuch <cortex@worlddomination.be>
2011-09-27 22:23:38 +0200

    [mod] set url field as unique, you can't add two time the same url

--- rp/models.py
+++ rp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class RP(models.Model):
     title = models.CharField(max_length=300)
-    url = models.URLField()
+    url = models.URLField(unique=True)
     published = models.BooleanField(default=False)
     note = models.IntegerField(default=0)
 
