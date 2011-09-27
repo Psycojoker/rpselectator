7af31a15ce456f0f24e4e477ba3552c56c558f85
Laurent Peuch <cortex@worlddomination.be>
2011-09-27 23:50:07 +0200

    [enh] use cbgv to add an custom edit form for each ticket

--- rp/urls.py
+++ rp/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView
 
 from models import RP
 
 urlpatterns = patterns('rp.views',
     url(r'^fill/$', 'fill', name="fill"),
+    url(r'^edit/(?P<pk>[0-9]+)/$', UpdateView.as_view(model=RP, success_url="/"), name="edit_rp"),
     url(r'^$',ListView.as_view(model=RP), name="rp_list"),
 )
