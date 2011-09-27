f1c2455fb6a6b5a49d875917c7265da86577f87a
Laurent Peuch <cortex@worlddomination.be>
2011-09-28 00:33:05 +0200

    [enh] add a langue field

--- rp/models.py
+++ rp/models.py
@@ -1,10 +1,12 @@
+# -*- coding:Utf-8 -*-
+
 from django.db import models
 
 class RP(models.Model):
     title = models.CharField(max_length=300, null=True, blank=True)
     url = models.URLField(unique=True)
     published = models.BooleanField(default=False)
-    note = models.IntegerField(default=0)
+    langue = models.IntegerField(max_length=2, choices=(('en', 'English'), ('fr', 'Français')), null=True)
 
     def __unicode__(self):
         return "%s - %s" % (self.title if self.title else "No title", self.url)
