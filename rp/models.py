24406f414d5177ac94cbe2773a102abc996ef9b3
Laurent Peuch <cortex@worlddomination.be>
2011-09-28 01:14:08 +0200

    [fix] stupid error, edited to fast: a choices field was a IntegerField instead of a CharField

--- rp/models.py
+++ rp/models.py
@@ -6,7 +6,7 @@ class RP(models.Model):
     title = models.CharField(max_length=300, null=True, blank=True)
     url = models.URLField(unique=True)
     published = models.BooleanField(default=False)
-    langue = models.IntegerField(max_length=2, choices=(('en', 'English'), ('fr', 'Français')), null=True)
+    langue = models.CharField(max_length=3, choices=(('EN', 'English'), ('FR', 'Français')), null=True, blank=True)
 
     def __unicode__(self):
         return "%s - %s" % (self.title if self.title else "No title", self.url)
