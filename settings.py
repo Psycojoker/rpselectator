34b49a00cfbab20032e6d91a3f519b9217f3a18d
Laurent Peuch <cortex@worlddomination.be>
2011-09-27 22:50:12 +0200

    [enh] set up admin interface

--- settings.py
+++ settings.py
@@ -120,10 +120,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rp',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
