0b5b8e8eacca6b58d88975f440533bf2178ecb8b
Laurent Peuch <cortex@worlddomination.be>
2011-09-28 00:16:13 +0200

    [mod] pep8

--- rp/urls.py
+++ rp/urls.py
@@ -8,6 +8,6 @@ from feeds import RSS
 urlpatterns = patterns('rp.views',
     url(r'^fill/$', 'fill', name="fill"),
     url(r'^edit/(?P<pk>[0-9]+)/$', UpdateView.as_view(model=RP, success_url="/"), name="edit_rp"),
-    url(r'^$',ListView.as_view(model=RP), name="rp_list"),
+    url(r'^$', ListView.as_view(model=RP), name="rp_list"),
     url(r'^rss.xml$', RSS(), name="rss"),
 )
