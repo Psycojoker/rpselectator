2fc887f9f08d72ecab501009f6c384689134984b
Laurent Peuch <cortex@worlddomination.be>
2011-09-28 00:54:13 +0200

    [enh] manage errors in fetching script

--- rp/views.py
+++ rp/views.py
@@ -1,5 +1,7 @@
 # Create your views here.
 
+from time import sleep
+
 from django.http import HttpResponse
 
 from models import RP
@@ -9,9 +11,21 @@ from urllib2 import urlopen
 def fill(request):
     def get_urls(url):
         a = 0
-        for url in urlopen(url):
-            url = url[:-1]
-            if not url.stip():
+
+        urls = ""
+        tries = 0
+        while not urls:
+            try:
+                urls = urlopen(url).read()
+            except Exception, e:
+                print "Malheur !: " + e
+                tries += 1
+                if tries > 3:
+                    return 0
+                sleep(1)
+
+        for url in urls.split("\n"):
+            if not url.strip():
                 continue
             if not RP.objects.filter(url=url):
                 RP.objects.create(url=url)
