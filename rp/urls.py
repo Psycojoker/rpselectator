e37db61de6c43caccb5b8c0f9c25f69d12fc92ee
Laurent Peuch <cortex@worlddomination.be>
2011-09-28 02:11:15 +0200

    [enh] move to a cbgv for the edit form

--- rp/urls.py
+++ rp/urls.py
@@ -5,9 +5,14 @@ from models import RP
 
 from feeds import RSS
 
+class RPEdit(UpdateView):
+    success_url="/"
+    model=RP
+
+
 urlpatterns = patterns('rp.views',
     url(r'^fill/$', 'fill', name="fill"),
-    url(r'^edit/(?P<pk>[0-9]+)/$', UpdateView.as_view(model=RP, success_url="/"), name="edit_rp"),
+    url(r'^edit/(?P<pk>[0-9]+)/$', RPEdit.as_view(), name="edit_rp"),
     url(r'^$', ListView.as_view(queryset=RP.objects.order_by('-id')), name="rp_list"),
     url(r'^rss.xml$', RSS(), name="rss"),
 )
