885e08ed7c9655dc20d2626943d91a5be7ec336e
Laurent Peuch <cortex@worlddomination.be>
2011-09-28 02:10:42 +0200

    [enh] add spanish in possible languages

--- rp/models.py
+++ rp/models.py
@@ -6,7 +6,7 @@ class RP(models.Model):
     title = models.CharField(max_length=300, null=True, blank=True)
     url = models.URLField(unique=True)
     published = models.BooleanField(default=False)
-    langue = models.CharField(max_length=3, choices=(('EN', 'English'), ('FR', 'Français')), null=True, blank=True)
+    langue = models.CharField(max_length=3, choices=(('EN', 'English'), ('FR', 'Français'), ('ES', 'Espagnol')), null=True, blank=True)
 
     def __unicode__(self):
         return "%s - %s" % (self.title if self.title else "No title", self.url)
