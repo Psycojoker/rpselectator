eb3c600b1821d749aab957c05c0739bf16da5ff5
Laurent Peuch <cortex@worlddomination.be>
2011-09-28 02:41:07 +0200

    [mod] move RPEdit cbgv to views.py

--- rp/urls.py
+++ rp/urls.py
@@ -1,52 +1,11 @@
-from re import sub
-from urllib2 import urlopen
-from mechanize import Browser
-from oice.langdet import langdet
-
 from django.conf.urls.defaults import patterns, url
-from django.views.generic import ListView, UpdateView
+from django.views.generic import ListView
 
 from models import RP
 
 from feeds import RSS
 
-def encode_sucks(text):
-    try:
-        return text.encode("Utf-8")
-    except UnicodeError:
-        pass
-    try:
-        return text.decode("Utf-8")
-    except UnicodeDecodeError:
-        pass
-    try:
-        return text.encode("iso-8859-1")
-    except:
-        pass
-    try:
-        return text.decode("iso-8859-1")
-    except:
-        pass
-    return "could not get the title :(, tell Bram that this website sucks"
-
-class RPEdit(UpdateView):
-    success_url="/"
-    model=RP
-
-    def get_form_kwargs(self, **kwargs):
-        data = super(RPEdit, self).get_form_kwargs(**kwargs)
-        if not data["instance"].title:
-            b = Browser()
-            b.open(data["instance"].url)
-            site = ".".join(map(lambda x: x.capitalize(), data["instance"].url.split("/")[2].replace("www.", "").split(".")[:-1]))
-            data["initial"]["title"] = "[%s] %s" % (site.encode("Utf-8"), encode_sucks(b.title()))
-        if not data["instance"].langue:
-            text = urlopen(data["instance"].url).read()
-            text = sub("[^\w ]", lambda x: "", text)
-            lang = langdet.LanguageDetector.detect(text).iso.upper()
-            if lang in ('FR', 'EN', 'ES'):
-                data["initial"]["langue"] = lang
-        return data
+from views import RPEdit
 
 urlpatterns = patterns('rp.views',
     url(r'^fill/$', 'fill', name="fill"),
--- rp/views.py
+++ rp/views.py
@@ -1,12 +1,15 @@
 # Create your views here.
 
 from time import sleep
+from re import sub
+from urllib2 import urlopen
+from mechanize import Browser
+from oice.langdet import langdet
 
+from django.views.generic import UpdateView
 from django.http import HttpResponse
 
 from models import RP
-
-from urllib2 import urlopen
 
 def fill(request):
     def get_urls(url):
@@ -34,5 +37,43 @@ def fill(request):
 
     a = 0
     a += get_urls("http://nurpa.be/log")
-    a += get_urls("http://nurpa.be/log-archive")
+    #a += get_urls("http://nurpa.be/log-archive")
     return HttpResponse("%s new rp items" % a)
+
+def encode_sucks(text):
+    try:
+        return text.encode("Utf-8")
+    except UnicodeError:
+        pass
+    try:
+        return text.decode("Utf-8")
+    except UnicodeDecodeError:
+        pass
+    try:
+        return text.encode("iso-8859-1")
+    except:
+        pass
+    try:
+        return text.decode("iso-8859-1")
+    except:
+        pass
+    return "could not get the title :(, tell Bram that this website sucks"
+
+class RPEdit(UpdateView):
+    success_url="/"
+    model=RP
+
+    def get_form_kwargs(self, **kwargs):
+        data = super(RPEdit, self).get_form_kwargs(**kwargs)
+        if not data["instance"].title:
+            b = Browser()
+            b.open(data["instance"].url)
+            site = ".".join(map(lambda x: x.capitalize(), data["instance"].url.split("/")[2].replace("www.", "").split(".")[:-1]))
+            data["initial"]["title"] = "[%s] %s" % (site.encode("Utf-8"), encode_sucks(b.title()))
+        if not data["instance"].langue:
+            text = urlopen(data["instance"].url).read()
+            text = sub("[^\w ]", lambda x: "", text)
+            lang = langdet.LanguageDetector.detect(text).iso.upper()
+            if lang in ('FR', 'EN', 'ES'):
+                data["initial"]["langue"] = lang
+        return data
