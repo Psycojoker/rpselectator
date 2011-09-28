f7f52c19696539d0188e8e12c37493cf6bc5ae22
Laurent Peuch <cortex@worlddomination.be>
2011-09-28 19:14:04 +0200

    [enh] list published items on /published/

--- rp/urls.py
+++ rp/urls.py
@@ -11,5 +11,6 @@ urlpatterns = patterns('rp.views',
     url(r'^fill/$', 'fill', name="fill"),
     url(r'^edit/(?P<pk>[0-9]+)/$', RPEdit.as_view(), name="edit_rp"),
     url(r'^$', ListView.as_view(queryset=RP.objects.order_by('-id')), name="rp_list"),
+    url(r'^published/$', ListView.as_view(queryset=RP.objects.filter(published=True).order_by('-id')), name="rp_list_published"),
     url(r'^rss.xml$', RSS(), name="rss"),
 )
