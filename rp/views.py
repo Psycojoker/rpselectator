1f5f80fde738200b242a1fb15d80078f39b5a01d
Laurent Peuch <cortex@worlddomination.be>
2011-09-28 02:45:34 +0200

    [enh] manage error case when the url doesn't exist

--- rp/views.py
+++ rp/views.py
@@ -2,7 +2,7 @@
 
 from time import sleep
 from re import sub
-from urllib2 import urlopen
+from urllib2 import urlopen, URLError
 from mechanize import Browser
 from oice.langdet import langdet
 
@@ -67,13 +67,19 @@ class RPEdit(UpdateView):
         data = super(RPEdit, self).get_form_kwargs(**kwargs)
         if not data["instance"].title:
             b = Browser()
-            b.open(data["instance"].url)
-            site = ".".join(map(lambda x: x.capitalize(), data["instance"].url.split("/")[2].replace("www.", "").split(".")[:-1]))
-            data["initial"]["title"] = "[%s] %s" % (site.encode("Utf-8"), encode_sucks(b.title()))
+            try:
+                b.open(data["instance"].url)
+                site = ".".join(map(lambda x: x.capitalize(), data["instance"].url.split("/")[2].replace("www.", "").split(".")[:-1]))
+                data["initial"]["title"] = "[%s] %s" % (site.encode("Utf-8"), encode_sucks(b.title()))
+            except URLError:
+                pass
         if not data["instance"].langue:
-            text = urlopen(data["instance"].url).read()
-            text = sub("[^\w ]", lambda x: "", text)
-            lang = langdet.LanguageDetector.detect(text).iso.upper()
-            if lang in ('FR', 'EN', 'ES'):
-                data["initial"]["langue"] = lang
+            try:
+                text = urlopen(data["instance"].url).read()
+                text = sub("[^\w ]", lambda x: "", text)
+                lang = langdet.LanguageDetector.detect(text).iso.upper()
+                if lang in ('FR', 'EN', 'ES'):
+                    data["initial"]["langue"] = lang
+            except URLError:
+                pass
         return data
