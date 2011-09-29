8f3926cfcb7da5333822d24485e59f9778285d37
Laurent Peuch <cortex@worlddomination.be>
2011-09-30 01:48:37 +0200

    [mod] don't displayed managed links on homepage

--- rp/urls.py
+++ rp/urls.py
@@ -10,7 +10,7 @@ from views import RPEdit
 urlpatterns = patterns('rp.views',
     url(r'^fill/$', 'fill', name="fill"),
     url(r'^edit/(?P<pk>[0-9]+)/$', RPEdit.as_view(), name="edit_rp"),
-    url(r'^$', ListView.as_view(queryset=RP.objects.order_by('-id')), name="rp_list"),
+    url(r'^$', ListView.as_view(queryset=RP.objects.filter(published=None).order_by('-id')), name="rp_list"),
     url(r'^published/$', ListView.as_view(queryset=RP.objects.filter(published=True).order_by('-id')), name="rp_list_published"),
     url(r'^archived/$', ListView.as_view(queryset=RP.objects.filter(published=False).order_by('-id')), name="rp_list_archived"),
     url(r'^rss.xml$', RSS(), name="rss"),
