2436255232a1e5ce4c452d126a75b59c722901e9
Laurent Peuch <cortex@worlddomination.be>
2011-09-29 21:10:45 +0200

    [enh] set up static files

--- settings.py
+++ settings.py
@@ -73,6 +73,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    PROJECT_PATH + '/static/',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
