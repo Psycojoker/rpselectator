ebac85b7f9a5152cf6a7b3e845214644d2d1dc3b
Laurent Peuch <cortex@worlddomination.be>
2011-09-30 01:48:47 +0200

    [mod] reset link to editing part and change editing url name

--- rp/urls.py
+++ rp/urls.py
@@ -9,7 +9,7 @@ from views import RPEdit
 
 urlpatterns = patterns('rp.views',
     url(r'^fill/$', 'fill', name="fill"),
-    url(r'^edit/(?P<pk>[0-9]+)/$', RPEdit.as_view(), name="edit_rp"),
+    url(r'^edit/(?P<pk>[0-9]+)/$', RPEdit.as_view(), name="rp_edit"),
     url(r'^$', ListView.as_view(queryset=RP.objects.filter(published=None).order_by('-id')), name="rp_list"),
     url(r'^published/$', ListView.as_view(queryset=RP.objects.filter(published=True).order_by('-id')), name="rp_list_published"),
     url(r'^archived/$', ListView.as_view(queryset=RP.objects.filter(published=False).order_by('-id')), name="rp_list_archived"),
