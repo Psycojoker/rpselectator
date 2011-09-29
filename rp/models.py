1d232d2c8e8f7fd58f19b518e47b1d3b1b5255a1
Laurent Peuch <cortex@worlddomination.be>
2011-09-30 01:06:12 +0200

    [mod] setting max title length to 90

--- rp/models.py
+++ rp/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 
 class RP(models.Model):
-    title = models.CharField(max_length=300, null=True, blank=True)
+    title = models.CharField(max_length=90, null=True, blank=True)
     url = models.URLField(unique=True)
     published = models.BooleanField(default=False)
     langue = models.CharField(max_length=3, null=True, blank=True)
