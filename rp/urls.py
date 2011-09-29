3ae68d8a185af6894cf1a521a79b2c5feea1c616
Laurent Peuch <cortex@worlddomination.be>
2011-09-30 01:36:52 +0200

    [enh] add an archived page

--- rp/urls.py
+++ rp/urls.py
@@ -12,5 +12,6 @@ urlpatterns = patterns('rp.views',
     url(r'^edit/(?P<pk>[0-9]+)/$', RPEdit.as_view(), name="edit_rp"),
     url(r'^$', ListView.as_view(queryset=RP.objects.order_by('-id')), name="rp_list"),
     url(r'^published/$', ListView.as_view(queryset=RP.objects.filter(published=True).order_by('-id')), name="rp_list_published"),
+    url(r'^archived/$', ListView.as_view(queryset=RP.objects.filter(published=False).order_by('-id')), name="rp_list_archived"),
     url(r'^rss.xml$', RSS(), name="rss"),
 )
