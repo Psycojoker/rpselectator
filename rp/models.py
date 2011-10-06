792414c95662342f36de2320af5344020d71614f
Laurent Peuch <cortex@worlddomination.be>
2011-10-07 00:34:46 +0200

    [enh] don't allow user to edit the datetime of RP

--- rp/models.py
+++ rp/models.py
@@ -7,7 +7,7 @@ class RP(models.Model):
     url = models.URLField(unique=True)
     published = models.NullBooleanField(default=None, null=True)
     langue = models.CharField(max_length=2, null=True, blank=True)
-    datetime = models.DateTimeField()
+    datetime = models.DateTimeField(editable=False)
 
     def __unicode__(self):
         return "%s - %s" % (self.title if self.title else "No title", self.url)
