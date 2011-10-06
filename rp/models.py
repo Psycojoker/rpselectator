e890ff91e2ddfc1f5fc42fcce6ea6acc4791f409
Laurent Peuch <cortex@worlddomination.be>
2011-10-07 00:52:52 +0200

    [enh] add a published_date to RP item, update it on save if the item is published and doesn't have one yet

--- rp/models.py
+++ rp/models.py
@@ -1,5 +1,7 @@
 # -*- coding:Utf-8 -*-
 
+from datetime import datetime
+
 from django.db import models
 
 class RP(models.Model):
@@ -8,6 +10,12 @@ class RP(models.Model):
     published = models.NullBooleanField(default=None, null=True)
     langue = models.CharField(max_length=2, null=True, blank=True)
     datetime = models.DateTimeField(editable=False)
+    published_date = models.DateTimeField(default=None, null=True, editable=False)
 
     def __unicode__(self):
         return "%s - %s" % (self.title if self.title else "No title", self.url)
+
+    def save(self, *args, **kwargs):
+        if self.published and not self.published_date:
+            self.published_date = datetime.now()
+        super(RP, self).save(*args, **kwargs)
