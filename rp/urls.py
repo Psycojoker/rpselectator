de75da4aa03f049aa1ff57597eaecc9ca7201257
Laurent Peuch <cortex@worlddomination.be>
2011-10-07 00:11:03 +0200

    [enh] protect the whole website except the rss with login_required

--- rp/urls.py
+++ rp/urls.py
@@ -1,17 +1,18 @@
 from django.conf.urls.defaults import patterns, url
 from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required
 
 from models import RP
 
 from feeds import RSS
 
-from views import RPEdit
+from views import RPEdit, fill
 
 urlpatterns = patterns('rp.views',
-    url(r'^fill/$', 'fill', name="fill"),
-    url(r'^edit/(?P<pk>[0-9]+)/$', RPEdit.as_view(), name="rp_edit"),
-    url(r'^$', ListView.as_view(queryset=RP.objects.filter(published=None).order_by('-id')), name="rp_list"),
-    url(r'^published/$', ListView.as_view(queryset=RP.objects.filter(published=True).order_by('-id')), name="rp_list_published"),
-    url(r'^archived/$', ListView.as_view(queryset=RP.objects.filter(published=False).order_by('-id')), name="rp_list_archived"),
+    url(r'^fill/$', login_required(fill), name="fill"),
+    url(r'^edit/(?P<pk>[0-9]+)/$', login_required(RPEdit.as_view()), name="rp_edit"),
+    url(r'^$', login_required(ListView.as_view(queryset=RP.objects.filter(published=None).order_by('-id'))), name="rp_list"),
+    url(r'^published/$', login_required(ListView.as_view(queryset=RP.objects.filter(published=True).order_by('-id'))), name="rp_list_published"),
+    url(r'^archived/$', login_required(ListView.as_view(queryset=RP.objects.filter(published=False).order_by('-id'))), name="rp_list_archived"),
     url(r'^rss.xml$', RSS(), name="rss"),
 )
--- urls.py
+++ urls.py
@@ -5,6 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^',include("rp.urls")),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
