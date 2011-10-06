7283c99776a2a9a3072f369bbd215dc495fed2e6
Laurent Peuch <cortex@worlddomination.be>
2011-10-07 00:32:19 +0200

    [enh] add a logout option and do the correct redirections on certain urls

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic import RedirectView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -6,6 +7,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^',include("rp.urls")),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name="logout"),
+    url(r'^accounts/$', RedirectView.as_view(url="/accounts/login/", permanent=False)),
+    url(r'^accounts/.*$', RedirectView.as_view(url="/", permanent=False)),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
