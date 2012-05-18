53f9ead73bbdb9e3d4b21b2369b746c8b9aee827
Laurent Peuch <cortex@worlddomination.be>
2012-05-18 19:08:39 +0200

    [enh] ajax POST of a modified article in the list view

--- rp/urls.py
+++ rp/urls.py
@@ -1,13 +1,25 @@
 from django.conf.urls.defaults import patterns, url
 from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.forms import ModelForm
 
 from models import RP
 from feeds import RSS
 from views import fill
 
+class RPForm(ModelForm):
+    class Meta:
+        model = RP
+
+def save(request):
+    form = RPForm(request.POST, instance=RP.objects.get(id=request.POST["id"]))
+    form.save()
+    return HttpResponse("ok")
+
 urlpatterns = patterns('rp.views',
     url(r'^fill/$', login_required(fill), name="fill"),
+    url(r'^save/$', login_required(save), name="save"),
     url(r'^$', login_required(ListView.as_view(queryset=RP.objects.filter(published=None).order_by('-id'))), name="rp_list"),
     url(r'^published/$', login_required(ListView.as_view(queryset=RP.objects.filter(published=True).order_by('-id'))), name="rp_list_published"),
     url(r'^archived/$', login_required(ListView.as_view(queryset=RP.objects.filter(published=False).order_by('-id'))), name="rp_list_archived"),
