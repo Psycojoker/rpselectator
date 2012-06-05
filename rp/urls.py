7510e04a1d7088c59ef5bd1b868c2c6e95930eac
Laurent Peuch <cortex@worlddomination.be>
2012-06-05 17:20:04 +0200

    [mod] revert sort order for lastest links

--- rp/urls.py
+++ rp/urls.py
@@ -39,7 +39,7 @@ def item_json(request, pk):
 urlpatterns = patterns('rp.views',
     url(r'^fill/$', login_required(fill), name="fill"),
     url(r'^save/$', login_required(save), name="save"),
-    url(r'^$', login_required(ListView.as_view(queryset=RP.objects.filter(published=None).order_by('-id'))), name="rp_list"),
+    url(r'^$', login_required(ListView.as_view(queryset=RP.objects.filter(published=None).order_by('id'))), name="rp_list"),
     url(r'^published/$', login_required(ListView.as_view(queryset=RP.objects.filter(published=True).order_by('-id'))), name="rp_list_published"),
     url(r'^archived/$', login_required(ListView.as_view(queryset=RP.objects.filter(published=False).order_by('-id'))), name="rp_list_archived"),
     url(r'^rss.xml$', RSS(), name="rss"),
