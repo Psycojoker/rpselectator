b1ed8cd687a288d6a4269f8dbe00e758672b7c85
pumpkin <pumpkin@mailoo.org>
2012-07-29 13:04:26 +0200

    Order rp_list by datetime (ASC/DESC) or published_date (DESC) instead of id

--- rp/urls.py
+++ rp/urls.py
@@ -39,9 +39,9 @@ def item_json(request, pk):
 urlpatterns = patterns('rp.views',
     url(r'^fill/$', login_required(fill), name="fill"),
     url(r'^save/$', login_required(save), name="save"),
-    url(r'^$', login_required(ListView.as_view(queryset=RP.objects.filter(published=None).order_by('id'))), name="rp_list"),
-    url(r'^published/$', login_required(ListView.as_view(queryset=RP.objects.filter(published=True).order_by('-id'))), name="rp_list_published"),
-    url(r'^archived/$', login_required(ListView.as_view(queryset=RP.objects.filter(published=False).order_by('-id'))), name="rp_list_archived"),
+    url(r'^$', login_required(ListView.as_view(queryset=RP.objects.filter(published=None).order_by('datetime'))), name="rp_list"),
+    url(r'^published/$', login_required(ListView.as_view(queryset=RP.objects.filter(published=True).order_by('-published_date'))), name="rp_list_published"),
+    url(r'^archived/$', login_required(ListView.as_view(queryset=RP.objects.filter(published=False).order_by('-datetime'))), name="rp_list_archived"),
     url(r'^rss.xml$', RSS(), name="rss"),
     url(r'^item_json/(?P<pk>\d+)/$', item_json, name="item_json"),
 )
